65cbf6e74de241524866558852ec08c328b36289
Paris <paris@sourcelair.com>
2015-09-16 12:08:42 +0300

    Add auto pip install

--- braintree_api/manage.py
+++ braintree_api/manage.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
 import os
+import subprocess
 import sys
+
+# HACK: Install dependencies if not already installed
+PIP_LOG = 'pip.install.log'
+if not os.path.exists(PIP_LOG):
+    with open(PIP_LOG, 'wb') as LOG_FILE:
+        subprocess.check_call(
+            ['pip', 'install', '-r', '../requirements.txt'],
+            stdout=LOG_FILE,
+            stderr=LOG_FILE
+        )
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "braintree_api.settings")
