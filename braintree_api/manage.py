255bee7449ca94b2e07bd6b28e8cfa06d6463d7c
Paris <paris@sourcelair.com>
2015-09-16 12:16:14 +0300

    Add pip install logs

--- braintree_api/manage.py
+++ braintree_api/manage.py
@@ -7,11 +7,13 @@ import sys
 PIP_LOG = 'pip.install.log'
 if not os.path.exists(PIP_LOG):
     with open(PIP_LOG, 'wb') as LOG_FILE:
+        print 'Installing dependencies...'
         subprocess.check_call(
             ['pip', 'install', '-r', '../requirements.txt'],
             stdout=LOG_FILE,
             stderr=LOG_FILE
         )
+        print 'Dependencies installed...'
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "braintree_api.settings")
